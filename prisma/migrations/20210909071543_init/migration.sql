6f0ae58b94987864e06f663f29539b0189160dbb
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-09-09 09:17:14 +0200

    prisma schema migration init

---
+++ prisma/migrations/20210909071543_init/migration.sql
@@ -0,0 +1,146 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `userId` INTEGER NOT NULL AUTO_INCREMENT,
+    `profileId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `User_profileId_unique`(`profileId`),
+    PRIMARY KEY (`userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UserProfile` (
+    `profileId` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `UserProfile_email_key`(`email`),
+    PRIMARY KEY (`profileId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Administrator` (
+    `adminId` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`adminId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UserForum` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `forumId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Forum` (
+    `forumId` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`forumId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `postId` INTEGER NOT NULL AUTO_INCREMENT,
+    `creatorId` INTEGER NOT NULL,
+    `forumId` INTEGER NOT NULL,
+    `contentId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Post_contentId_unique`(`contentId`),
+    PRIMARY KEY (`postId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Content` (
+    `contentId` INTEGER NOT NULL AUTO_INCREMENT,
+    `postMessage` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`contentId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ContentUrl` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `contentUrl` VARCHAR(191) NOT NULL,
+    `contentId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `ContentUrl_contentId_unique`(`contentId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ContentImage` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `image_path` VARCHAR(191) NOT NULL,
+    `contentId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `ContentImage_contentId_unique`(`contentId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UserLike` (
+    `likeId` INTEGER NOT NULL AUTO_INCREMENT,
+    `likerId` INTEGER NOT NULL,
+    `postId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`likeId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `commentId` INTEGER NOT NULL AUTO_INCREMENT,
+    `commenterId` INTEGER NOT NULL,
+    `postId` INTEGER NOT NULL,
+    `commentMessage` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`commentId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_profileId_fkey` FOREIGN KEY (`profileId`) REFERENCES `UserProfile`(`profileId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Administrator` ADD CONSTRAINT `Administrator_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserForum` ADD CONSTRAINT `UserForum_forumId_fkey` FOREIGN KEY (`forumId`) REFERENCES `Forum`(`forumId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserForum` ADD CONSTRAINT `UserForum_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_creatorId_fkey` FOREIGN KEY (`creatorId`) REFERENCES `User`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_forumId_fkey` FOREIGN KEY (`forumId`) REFERENCES `Forum`(`forumId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_contentId_fkey` FOREIGN KEY (`contentId`) REFERENCES `Content`(`contentId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ContentUrl` ADD CONSTRAINT `ContentUrl_contentId_fkey` FOREIGN KEY (`contentId`) REFERENCES `Content`(`contentId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ContentImage` ADD CONSTRAINT `ContentImage_contentId_fkey` FOREIGN KEY (`contentId`) REFERENCES `Content`(`contentId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserLike` ADD CONSTRAINT `UserLike_likerId_fkey` FOREIGN KEY (`likerId`) REFERENCES `User`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserLike` ADD CONSTRAINT `UserLike_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`postId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_commenterId_fkey` FOREIGN KEY (`commenterId`) REFERENCES `User`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`postId`) ON DELETE RESTRICT ON UPDATE CASCADE;
