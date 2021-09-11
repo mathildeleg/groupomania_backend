28f41a942ca28e75a20c433ba43c6fb231398165
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-09-11 17:45:36 +0200

    add middleware token

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+// create token for users
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'user Id non valable';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({error: error | 'Requête non authentifiée '});
+  }
+};
