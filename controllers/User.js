89492e06c9d4f7ea456afa645315922cb9623c39
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-09-16 15:55:54 +0200

    add password-validator to check password

--- controllers/User.js
+++ controllers/User.js
@@ -1,12 +1,27 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const passwordValidator = require('password-validator');
 
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
+// schema for password validation to ensure password has those conditions
+const schema = new passwordValidator();
+schema
+    .is().min(8)                                    // Minimum length 8
+    .is().max(100)                                  // Maximum length 100
+    .has().uppercase()                              // Must have uppercase letters
+    .has().lowercase()                              // Must have lowercase letters
+    .has().digits(2)                                // Must have at least 2 digits
+    .has().not().spaces()                           // Should not have spaces
+    .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+
 exports.signup = async (req, res) => {
     const userProfile = req.body.userProfile;
     const { email, password, firstName, lastName, avatar } = userProfile;
+    if(!schema.validate(password)){
+        return res.status(401).json({ error: 'Mot de passe non valide !' })
+    }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt)
     try {
