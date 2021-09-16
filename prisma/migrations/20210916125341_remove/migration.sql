0ca7bde57939a42931f15fa19d59d7fd99728715
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-09-16 14:55:53 +0200

    tried adding a column for like but useless

---
+++ prisma/migrations/20210916125341_remove/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `like` on the `UserLike` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `UserLike` DROP COLUMN `like`;
