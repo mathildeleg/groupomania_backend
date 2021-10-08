8147f1a034d8cbb5b4113ed98444d1ab674d562b
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-10-08 17:39:29 +0200

    modify profile routes so that id = req.userId

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -1,32 +1,55 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
+function formatProfile(prismaProfile){
+    const { email, firstName, lastName, avatar } = prismaProfile.userProfile;
+    const newProfile = { email, firstName, lastName, avatar };
+    return newProfile;
+}
+
 // route to get a user profile
 exports.getOneProfile = async (req, res, next) => {
-    // get id of profile
-    const id = req.params.id;
-    // find profile with its id
-    const profile = await prisma.userProfile.findUnique({
-        where: {
-            profileId: Number(id),
-        }
+    // get id of user currently connected
+    const id = req.userId;
+    // get profile of user connected
+    const profile = await prisma.user.findUnique({
+        where: { 
+            userId: Number(id),
+        },
+        select: {
+            userProfile: {
+                select: {
+                    email: true,
+                    firstName: true,
+                    lastName: true,
+                    avatar: true,
+                }
+            },
+        },
     });
-    res.json(profile);
+    // return profile with correct format
+    res.json(formatProfile(profile));
 };
 
 // route to update a profile
 exports.updateProfile = async (req, res, next) => {
     // get id of profile
-    const id = req.params.id;
-    const { email, avatar } = req.body;
+    const id = req.userId;
+    const { email, avatar, firstName, lastName } = req.body;
     // update only email and avatar of profile
-    const updatedProfile = await prisma.userProfile.update({
+    const updatedProfile = await prisma.user.update({
         where: {
-            profileId: Number(id),
+            userId: Number(id),
         },
         data: {
-                email,
-                avatar,
+            userProfile: {
+                update: {
+                    email,
+                    firstName,
+                    lastName,
+                    avatar,
+                }
+            }
         },
     });
     res.json(updatedProfile);
@@ -35,12 +58,15 @@ exports.updateProfile = async (req, res, next) => {
 // route to delete a profile
 exports.deleteProfile = async (req, res, next) => {
     // get id of profile
-    const id = req.params.id;
+    const id = req.userId;
     // delete profile corresponding
-    const deletedProfile = await prisma.userProfile.delete({
+    const deletedProfile = await prisma.user.delete({
         where: {
-            profileId: Number(id),
+            userId: Number(id),
         },
+        // select: {
+        //     userProfile: true,
+        // },
     });
     res.json(deletedProfile);
 }
--- routes/Profile.js
+++ routes/Profile.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 const profileCtrl = require('../controllers/Profile');
 
-router.get('/:id', profileCtrl.getOneProfile);
-router.put('/:id', profileCtrl.updateProfile);
-router.delete('/:id', profileCtrl.deleteProfile);
+router.get('/me', auth, profileCtrl.getOneProfile);
+router.put('/me/update', auth, profileCtrl.updateProfile);
+router.delete('/me/delete', auth, profileCtrl.deleteProfile);
 
 module.exports = router;
