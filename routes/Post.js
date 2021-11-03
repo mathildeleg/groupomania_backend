3abbeb8d706b8196b1b3b6d0db7b266d60c77ca5
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-11-03 15:21:33 +0100

    remove multer since images are links

--- routes/Post.js
+++ routes/Post.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
 
 const postCtrl = require('../controllers/Post');
 
 router.get('/forum/:forumId/post', auth, postCtrl.getAllPosts);
-router.post('/forum/:forumId/post', auth, multer, postCtrl.createPost);
+router.post('/forum/:forumId/post', auth, postCtrl.createPost);
 router.get('/forum/:forumId/post/:postId', auth, postCtrl.getOnePost);
 router.put('/forum/:forumId/post/:postId', auth, postCtrl.updatePost);
 router.delete('/forum/:forumId/post/:postId', auth, postCtrl.deletePost);
